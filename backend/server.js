8e65c5cc7ede9e14211366b52a0eb645b6fb7278
m1hammad <hammad27r@hotmail.com>
2023-02-04 14:40:36 -0500

    initializing backend api environment

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require('dotenv').config();
+
+const PORT = process.env.PORT
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect(process.env.mongoDB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+},
+() => console.log(`connected to mongodb at ${process.env.mongoDB_URL}`))
+
+app.listen(PORT, () => console.log(`App is running on ${PORT}`))
